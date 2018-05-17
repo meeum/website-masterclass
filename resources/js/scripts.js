4ddcd8d170d5b4979a8448de887e0319ee59234c
DESKTOP-4HVL62P\sam <sam@meeum.com>
2018-05-17 12:16:04 +1000

    active nav classes

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,3 +1,23 @@
 
 
-$('iframe').wrap('<div class="iframe-container"></div>');
+// nav
+function active_menu() {
+    var url = window.location.href;
+    $(".nav-header a")
+      .filter(function() {
+        return this.href == url;
+      })
+      .addClass("active");
+}
+
+active_menu();
+// add a body class
+$(bodyClass()).addClass('cock');
+
+function bodyClass() {
+    return 'body';
+}
+
+
+//iframe
+$("iframe").wrap('<div class="iframe-container"></div>');
