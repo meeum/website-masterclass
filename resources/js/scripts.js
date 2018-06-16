b9bebd987efc80cc95e5fcea642108f3c07d438a
meeum <sam@meeum.com>
2018-06-17 09:32:12 +1000

    added vars call to js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,61 +1,54 @@
 
+// call polyfill so IE will read custom properties
+cssVars();
 
-// nav
+
+// open mobile menu
+$('.nav-prompt').click(function () {
+
+  if ($('.nav-inner').attr('aria-expanded') === 'false') {
+    $('.nav-inner').attr('aria-expanded', 'true');
+  } else {
+    $('.nav-inner').attr('aria-expanded', 'false');
+  };
+
+
+  if ($('.nav-prompt').text() === 'Open Navigation') {
+    $('.nav-prompt').text('Close Navigation');
+  } else {
+    $(".nav-prompt").text("Open Navigation");
+  }
+})
+
+
+// active class on nav
 function active_menu() {
-    var url = window.location.href;
-    $(".nav-inner a")
-      .filter(function() {
-        return this.href == url;
-      })
-      .addClass("active");
+  var url = window.location.href;
+  $(".nav-inner a")
+    .filter(function () {
+      return this.href == url;
+    }).addClass("active");
 }
 
 active_menu();
 
 
-
-// open mobile menu
-$(".nav-prompt").click(function () {
-  $(".nav-inner").attr("aria-expanded", function(index, attr) {
-    return attr == "true" ? "false" : "true";
-  });
-
-  // if ($(".nav-prompt").text() == "Open Navigation") {
-  //   $(this).text("Close Navigation").addClass('nav-is-open').removeClass('nav-is-closed');
-  // } else {
-  //   $(this).text("Open Navigation").addClass("nav-is-closed").removeClass("nav-is-open");;
-  // }; 
-
-  if ($(".nav-prompt").text() == "Open Navigation") {
-    $(this).text("Close Navigation");
-  } else {
-    $(this).text("Open Navigation");
-  }; 
-
-  
-});
-
-
-
-
 //iframe
+// function iframeFix() {
+//   $("iframe").wrap('<div class="iframe-container"></div>');
+// }
 
+// iframeFix();
 
-function iframeFix() {
-  $("iframe").wrap('<div class="iframe-container"></div>');
-}
-
-iframeFix();
 
 // image gallery
+// gallery http://dbrekalo.github.io/simpleLightbox/
 function imageGallery() {
   if (!$(".image-gallery").length) {
     return;
   }
-  // gallery http://dbrekalo.github.io/simpleLightbox/
-
+  
   $(".image-gallery a").simpleLightbox();
-
 }
 
 imageGallery();
